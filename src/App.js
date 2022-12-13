7f2ceb3876b95c4308951037f13b96fe729a3b9f
TaftSound <mattmurphytattoos@gmail.com>
2022-12-13 15:06:39 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      
+    }
+  }
+  
+  render () {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+
+export default App;
