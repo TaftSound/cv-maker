b7f3d3f711be46df94cd0539952b16870d629da3
TaftSound <mattmurphytattoos@gmail.com>
2022-12-15 09:34:12 -0800

    Create work experince boilerplate component

---
+++ src/components/work-experience.js
@@ -0,0 +1,18 @@
+import { Component } from "react";
+import GenericSection from "./generic-section";
+
+class WorkExperience extends Component {
+  constructor (props) {
+    super (props)
+
+    this.state = {
+
+    }
+  }
+
+  render () {
+    return <div></div>
+  }
+}
+
+export default WorkExperience
